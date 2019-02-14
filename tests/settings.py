34d38b94b1fd7f1be378e979a152329e4df99086
Jonathan Loo <jloo@travelanalytics.com.au>
2019-02-14 15:01:56 +1100

    are those needed

---
+++ tests/settings.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8
+from __future__ import unicode_literals, absolute_import
+
+import django
+
+DEBUG = True
+USE_TZ = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sites",
+    "autocomplete_chained",
+]
+
+SITE_ID = 1
+
+MIDDLEWARE = ()
